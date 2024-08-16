d4f22bc89f96172ed7bb91b4c1e6ce8742775337
FPS\pranjal.negi <pranjal.negi>
2024-08-16 12:26:36 +0530

    microservices project

---
+++ spring-boot-microservices-master/spring-boot-microservices-master/inventory-service/src/main/java/com/programming/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.programming.inventoryservice.repository;
+
+import com.programming.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
